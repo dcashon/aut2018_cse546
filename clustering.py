11b19d75ac5921efba5e1a920ffa8d53e95aab4c
DCashon <dcashon@uw.edu>
2018-11-07 21:15:47 -0800

    k-means second

--- clustering.py
+++ clustering.py
@@ -27,9 +27,10 @@ def generate_partitions(num_points, num_partitions):
             partitions.append(to_shuffle[sum(counts[:i]): sum(counts[:i]) + counts[i]])
     return partitions, counts
 
-def solve_naive_kmeans(data, k, tol, max_steps):
+def solve_kmeans_naive(data, k, tol, max_steps):
     """
     Naively solves the k-means problem for k clusters given n by d data
+    Terrible idea
     Reference:
     https://nlp.stanford.edu/IR-book/html/htmledition/k-means-1.html
     Args:
@@ -47,20 +48,61 @@ def solve_naive_kmeans(data, k, tol, max_steps):
     # set up variables
     cluster_means, f_val = [], 100
     while abs(f_val) > tol:
-        f_val = 0
+        cluster_means, f_val = [], 0
         # Generate k random disjoint partitions
         partitions, counts = generate_partitions(data.shape[0], k)
         for subsets in partitions:
             # calculate the centroid of current subset
-            cluster_means.append(np.sum(data[subsets::], axis=0) / np.linalg.norm(np.array(subsets),2))
+            cluster_means.append(np.sum(data[subsets], axis=0) / np.linalg.norm(np.array(subsets),2))
         # calculate objective function value
-        for k in range(0, len(cluster_means)):
-            for vecs in subsets[k]:
-                f_val += np.linalg.norm((vecs - cluster_means[k]),2)**2
+        for idx in range(0, len(cluster_means)):
+            for vecs in subsets:
+                f_val += np.linalg.norm((data[vecs] - cluster_means[idx]),2)**2
         # DEBUG
         print(f_val)
 
-
+def solve_kmeans_lloyd(data, k, tol):
+    """
+    Solve for k clusters using Lloyd's algorithm
+    Reference:
+    https://medium.com/@kshithappens/local-maxima-2abb717d6c06
+    Args:
+        -data: n by d np.array
+        -k:    number of clusters desired
+    :param data:
+    :param k:
+    :param tol:
+    :return:
+    """
+    # variables
+    centers = np.zeros((k, data.shape[1]))
+    centers_prior = np.zeros((k, data.shape[1])) # may not need to allocate this
+    clusters, delta = {}, 10000
+    for j in range(0,k):
+        clusters[str(j)] = []
+    # initialize with k random centers drawn from n, no need to worry about replacement here n >> k
+    centers[:] = data[np.random.randint(0, data.shape[0], k)]
+    # assign each data point to the nearest center
+    while delta > tol:
+        for idx in range(0, data.shape[0]):
+            d_min = 10000
+            for i in range(0,k):
+                # calc distance to centers
+                d = np.linalg.norm(data[idx] - centers[i], 2)
+                if d < d_min:
+                    d_min = d
+                    min_center = i
+            clusters[str(min_center)].append(idx)
+        # reset centers and recalculate
+        centers_prior[:] = centers[:]
+        centers[:] = 0
+        for key in clusters.keys():
+            centers[int(key)] = np.sum(data[clusters[key]], axis=0) / np.linalg.norm(np.array(clusters[key]))
+            clusters[key] = []
+        centers = np.nan_to_num(centers)
+        delta = np.max(np.abs(centers - centers_prior))
+        print(delta)
+        # calculate the objective function
 
 
 
